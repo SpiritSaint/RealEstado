75762a1baf23870e67ff58587d6abe63dd4c7be6
Ian Torres <iantorres@outlook.com>
2022-11-05 20:48:33 -0300

    Code added.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +14,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()
+            ->create([
+                'name' => 'Ian Torres',
+                'email' => 'iantorres@outlook.com',
+                'tax' => fake()->uuid(),
+                'role' => 'creator',
+            ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(10)
+            ->create();
     }
 }
